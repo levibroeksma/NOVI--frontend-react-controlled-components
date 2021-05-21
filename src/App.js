7d9c494293411335cfa0dd9ed63bc432a43ea3ed
School <School@Levis-oude-bakbeest.local>
2021-05-21 11:26:49 +0200

    solved bonus assignments

--- src/App.js
+++ src/App.js
@@ -2,53 +2,73 @@ import React from 'react';
 import './App.css';
 
 function App() {
-    const [nameValue, setNameValue] = React.useState('');
-    const [ageValue, setAgeValue] = React.useState('');
-    const [foundValue, setFoundValue] = React.useState('');
-    const [reviewValue, setReviewValue] =React.useState('');
-    // const onSubmit = data => console.log(data);
+    // const [nameValue, setNameValue] = React.useState('');
+    // const [ageValue, setAgeValue] = React.useState('');
+    // const [foundValue, setFoundValue] = React.useState('');
+    // const [reviewValue, setReviewValue] =React.useState('');
+
+    const [valueInput, setValueInput] = React.useState({
+        name: "",
+        age: "",
+        foundUs: "",
+        yourReview: ""
+    })
+
+    // Set criteria for submitting the form
     const submitHandler = (event) => {
+        // disable default settings, we don't want the page to refresh on submitting.
         event.preventDefault();
-        const pushDataToConsole = {
-            name: nameValue,
-            age: ageValue,
-            locationFound: foundValue,
-            yourReview: reviewValue
-        }
+        // Create an object which we push to the
+        const pushDataToConsole = {name: valueInput.name, age: valueInput.age, foundUs: valueInput.foundUs, yourReview: valueInput.yourReview};
         console.log(pushDataToConsole);
     };
+
+    // onChange function:
+
+    function handleChange(e) {
+        const value = e.target.value;
+        setValueInput({
+            ...valueInput,
+            [e.target.name]: value,
+            [e.target.age]: value,
+            [e.target.foundUs]: value,
+            [e.target.yourReview]: value
+        });
+    }
+
   return (
     <div>
         <form onSubmit={submitHandler}>
             <fieldset>
-                <legend>Gegevens</legend>
+                <h2>Gegevens</h2>
                 <label htmlFor="name">Naam:</label>
                 <input
                     type="text"
                     id="name"
                     name="name"
-                    value={nameValue}
-                    onChange={(e) => setNameValue(e.target.value)}
+                    value={valueInput.name}
+                    onChange={handleChange}
                 />
                 <label htmlFor="age">Leeftijd:</label>
                 <input
                     id="name"
                     type="number"
+                    name="age"
                     placeholder="0"
-                    value={ageValue}
-                    onChange={(e) => setAgeValue(e.target.value)}
+                    value={valueInput.age}
+                    onChange={handleChange}
                 />
             </fieldset>
             <fieldset>
-                <legend>Jouw review</legend>
+                <h2>Jouw review</h2>
                 <label htmlFor="howDidYouFindUs">
                     Hoe heb je dit recept gevonden?
                 </label>
                 <select
-                    name="howDidYouFindUs"
+                    name="foundUs"
                     id="howDidYouFindUs"
-                    value={foundValue}
-                    onChange={(e) => setFoundValue(e.target.value)}
+                    value={valueInput.foundUs}
+                    onChange={handleChange}
                 >
                     <option value="" disabled>Kies een opties...</option>
                     <option value="google">Google</option>
@@ -59,13 +79,13 @@ function App() {
                 <label htmlFor="review">
                     Opmerkingen:
                     <textarea
-                        name="review"
+                        name="yourReview"
                         id="review"
                         cols="30"
                         rows="5"
                         placeholder="Wat vond je van het recept?"
-                        value={reviewValue}
-                        onChange={(e) => setReviewValue(e.target.value)}
+                        value={valueInput.yourReview}
+                        onChange={handleChange}
                     >
                     </textarea>
                 </label>
